d4a0653d0be7604edb1dd2b1c9e84439319a88d8
user <ezer4611@gmail.com>
2022-11-23 10:49:33 +0900

    증감연산자

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -8,6 +8,25 @@ public class Test {
 		System.out.println(a);
 		
 		System.out.println(a++); //출력 후 증가
+		
+		
+		/* <증감연산자>
+		a++ : 후위연산자
+		++a : 전위연산자
+		
+		int a=3;
+		a++;
+		System.out.println(a);
+		
+		int b=3;
+		++b;
+		System.out.println(b);
+		
+		전위, 후위 증감 연산자를 단독으로 쓰는 경우는 1증가 감소란 뜻
+		
+		단독으로 쓸 경우가 아닐때는 순서를 생각해서 코드짜야함
+		=연산자랑 같이 쓸때는 순서를 생각해주어야 한다
+		*/
 	}
 
 }
