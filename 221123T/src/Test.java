c39a2ea369e4df43f21603d0e25c7dded4c06c9c
user <ezer4611@gmail.com>
2022-11-23 18:17:06 +0900

    switch-case 예제1

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -5,30 +5,22 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		System.out.println("점수 입력:");
 		
-		int kor = s.nextInt();
-		char grade;
+		System.out.println("무슨 요일이죠?");
+		String day = s.next();
 		
-		if (kor>=90) {
-			grade = 'A';
-		}
-		else if (kor>=80) {
-			grade = 'B';
-		}
-		else if (kor>=70) {
-			grade = 'C';
-		}
-		else {
-			grade = 'F';
-		}
+		//내가 입력한 요일을 조건으로 인식
 		
-		System.out.println("성적: " + grade);
-		
-		//조건연산자로 표현
-		char a = kor>=90?'A':'B';
-		System.out.println("성적: " + a);
+		switch(day) { //입력값이 문자열
+		case "수요일" :
+			System.out.println("수");
+			break; //break 없다면 다음 case문으로 계속 넘어감
+		case "목요일" :
+			System.out.println("목");
+			break;
+		default: //case의 값이 다 없을때 실행되는 default문
+			System.out.println("수,목 아님");
+			break;
+		}
 	}
-	
-
 }
