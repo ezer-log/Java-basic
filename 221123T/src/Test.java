4198e02ac6c9961388d12085923e6f27219bc9fe
user <ezer4611@gmail.com>
2022-11-23 15:44:33 +0900

    조건연산자 vs 조건문

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -4,19 +4,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		int a=10, b=4;
-
-		//중첩조건
-		if(a==10) {
-			System.out.println("A");
+		//조건연산자 ?: 는 if-else로 바꿀 수 있음
+		
+		//조건 연산자
+		i = a>b?a-b:b-a;
+		
+		//조건문
+		if (a>b) {
+			i = a - b;
 		}
-		else if(a<b) {
-			System.out.println("B");
+		else {
+			i = b - a;
 		}
-		else if(a>b) {
-			System.out.println("C");
-		}
-
-	}	
+	}
 
 }
