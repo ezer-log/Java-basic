b1ef390ded20b5c7526d9f66b06b4419d12836cc
user <ezer4611@gmail.com>
2022-11-23 15:27:31 +0900

    중첩 조건문

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -4,21 +4,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Scanner s = new Scanner(System.in);
-		
-		System.out.println("정수입력:");
-		//정수로 입력받아라.
-		
-		int n = s.nextInt();
-		System.out.println("정수는 " + n);
-		
-		if (n%2==0) {
-			System.out.println("짝수, 2의 배수이다");
+		int a=10, b=4;
+
+		//중첩조건
+		if(a==10) {
+			System.out.println("A");
 		}
-		else {
-			System.out.println("홀수, 2의 배수가 아니다");
+		else if(a<b) {
+			System.out.println("B");
 		}
-		
+		else if(a>b) {
+			System.out.println("C");
+		}
 
 	}	
 
