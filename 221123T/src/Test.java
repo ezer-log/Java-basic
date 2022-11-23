e1899b7e0c474650ba2b3de592f3796573a82c78
user <ezer4611@gmail.com>
2022-11-23 12:34:28 +0900

    Scanner 클래스 개념

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -1,28 +1,23 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
 		/*
-		 * <조건 연산자>
+		 * <Scanner 클래스>
 		 * 
-		 * a?b:c
-		 * a라는 조건이 
-		 * 참이면 b
-		 * 거짓이면 c
-		 * 리턴됨
+		 * System.in 에게 키를 읽게 하고, 읽은 바이트를 문자, 정수, 실수, 불리언, 문자열 등 다양한 타임으로 변환하여 리턴
+		 * Scanner a = new Scanner(System.in);
 		 * 
-		 *     조건  ?   참  : 거짓
-		 * 
-		 * int s = (x>y)?1:-1;
-		 * 리턴값에 따라 대입됨
+		 * import문을 필요로 한다.
+		 * import java.util.Scanner;
 		 * 
 		 */
-
 		
-		int a=3, b=5;
-		System.out.println( (a>b) ? (a+b) : (a-b));
-		
+		Scanner s = new Scanner(System.in);
+		//System.in에게 키보드값을 읽게 하기 위해 Scanner객체 생성해야 함
+		//클래스명 객체명 = new 클래스명
 		
 	}	
 
