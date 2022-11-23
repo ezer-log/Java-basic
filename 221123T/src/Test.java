34327ca4ee09670ecdfcef0f065f907987176458
user <ezer4611@gmail.com>
2022-11-23 12:47:33 +0900

    Scanner 사용 예시

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -19,6 +19,26 @@ public class Test {
 		//System.in에게 키보드값을 읽게 하기 위해 Scanner객체 생성해야 함
 		//클래스명 객체명 = new 클래스명
 		
+		System.out.println("여러분 이름 입력해주세요");
+		String name = s.next(); //s는 scanner 객체
+		System.out.println("내 이름은 "+ name);
+		//이렇게 하면 콘솔창에 이름을 적을 수 있다!!!!!!! 엔터하면 출력됨...!!!
+		//파이썬의 input 받는거랑 비슷한건가..?
+		
+		System.out.println("여러분 나이 입력해주세요");
+		int age = s.nextInt();
+		System.out.printf("내 나이는 %d살\n", age);
+		
+		System.out.println("여러분 키 입력해주세요");
+		double height = s.nextDouble();
+		System.out.println("내 키는 "+ height);
+		
+		Scanner s2 = new Scanner(System.in);
+		System.out.println("두 정수 입력하세요");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println(a+b);
+		
 	}	
 
 }
