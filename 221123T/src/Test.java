a6e061d19b33e08f580e44229bb93ca81d0ea583
user <ezer4611@gmail.com>
2022-11-23 11:58:30 +0900

    강제 타입변환 응용

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -3,24 +3,46 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		String name="건용";
-		int age=20;
-		double ki=177.7;
-		char blood='O';
+		//영어 100, 국어 95점으로 초기화
+		
+		int eng = 100;
+		int kor = 95;
+		
+		System.out.println(eng);
+		System.out.println(kor);
+		
+		//실수 float 보다는 double 많이 씀
+		double avg = (eng + kor)/2;
+		
+		System.out.println(avg); 
+		
 		
 		/*
-		 * printf와 println을 이용해 출력하여라.
-		 * 건용의 나이는 20살이다.
-		 * 건용의 혈액형은 O형
+		 * 소수점이 나오긴 하지만 계산이 이상하다.
+		 * 정수 나누기 정수를 했고,
+		 * 그런 과정에서 소수점이 버려짐
+		 * 
+		 * 97.5가 나와야 하는데 0.5가 버려진것.
+		 * 나눗셈 둘중 하나를 실수로 바꿔줘야 한다
 		 */
 		
-		System.out.println(name+"의 나이는 " +age+ "살이다");
-		System.out.printf("%s의 나이는 %d살이다\n", name, age); // "\n" 은 엔터 역할
 		
-		System.out.println(name + "의 나이는 " + age + "살이다. \n키는 " 
-		+ ki + "cm이며 " + name +"의 혈액형은 " + blood + "형이다.");
+		//1. 뒤에 나누는 숫자를 실수로 바꾸기
+		double avg1 = (eng + kor)/2.0;
+		System.out.println(avg1); 
 		
-		System.out.printf("%s의 나이는 %d이다.%n%s의 혈액형은 %c형%n",name,age,name,blood);
+		//2. 앞에 나눠지는 숫자를 실수로 바꾸기 (강제 타입변환)
+		double avg2 = (double)(eng + kor)/2;
+		System.out.println(avg2);
+		
+		//printf 로 출력해보기
+		double avg3 = (double)(eng + kor)/2;
+		System.out.printf("두 과목 평균은 %.1f점이다\n", avg3);
+		System.out.println("두 과목 평균은 " +avg3 + "점이다");
+		
+
+		
+		
 	}	
 
 }
