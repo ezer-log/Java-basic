6f2a5dd5c95359ff7fbf382fcd3eaed3ef814f45
user <ezer4611@gmail.com>
2022-11-23 10:59:34 +0900

    증감연산자 순서

--- 221123T/src/Test.java
+++ 221123T/src/Test.java
@@ -3,30 +3,28 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		int a=0;
-		a+=3; //a=a+3
-		System.out.println(a);
+		int a,b = 4;
+		a = b++;
+
+		System.out.println(a); //4
+		System.out.println(b); //5
 		
-		System.out.println(a++); //출력 후 증가
-		
-		
-		/* <증감연산자>
-		a++ : 후위연산자
-		++a : 전위연산자
-		
-		int a=3;
-		a++;
-		System.out.println(a);
-		
-		int b=3;
-		++b;
-		System.out.println(b);
-		
-		전위, 후위 증감 연산자를 단독으로 쓰는 경우는 1증가 감소란 뜻
-		
-		단독으로 쓸 경우가 아닐때는 순서를 생각해서 코드짜야함
-		=연산자랑 같이 쓸때는 순서를 생각해주어야 한다
+		/* 
+		 <증감연산자>
+		 1증가가 먼저냐, 대입이 먼저냐를 봐야한다
+		 후위 연산자이기 때문에, 대입을 먼저 한 후 나중에 증가시키겠다는 뜻.
 		*/
+		
+		int c,d = 4;
+		c = ++d;
+		
+		System.out.println(c); //5
+		System.out.println(d); //5
+		
+		/* 
+		 전위 연산자이기 때문에, 증가 시키고 대입.
+		*/
+		
 	}
 
 }
