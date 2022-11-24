1d493f40837c7fbf4a1a65e0c3b940591a282cf7
ezer-log <ezer4611@gmail.com>
2022-11-24 10:34:45 +0900

    Math.random 함수 - 특정 범위안 정수 뽑기

--- 221124R/src/Test.java
+++ 221124R/src/Test.java
@@ -3,41 +3,27 @@ import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) { 
-
-		Scanner s = new Scanner(System.in);
-
-		System.out.print("글자 입력 : ");
-
-		char ch = s.next().charAt(0);
-
-		switch(ch) {
-
-			case 'm':
-
-				System.out.println("movie");
-
-				break;
-
-			case 's':	
-			
-				System.out.println("sing");
-
-				break;
-
-			case 'b':
-
-				System.out.println("book");
-
-				break;
-
-			default:
-
-				System.out.println("etc");
-
-				break;
-
+		
+		int n = (int) (Math.random()*9) +4;
+		//4~12까지의 수 중에서 랜덤으로 값을 뽑아냄
+		
+		
+		//1이 뽑아지면 "one"
+		//5가 뽑아지면 "five"
+		//6이 뽑아지면 "six"
+		
+		switch(n) {
+		case 1:
+			System.out.println("one");
+			break;
+		case 5:
+			System.out.println("five");
+			break;
+		case 6:
+			System.out.println("six");
+			break;
 		}
-
-		}
+		System.out.println(n);
+	}
 
 }
