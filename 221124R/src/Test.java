6bc848d6f19807ac2d9da7f67f5df435fde0fe4d
ezer-log <ezer4611@gmail.com>
2022-11-24 11:44:49 +0900

    for문 - 5행짜리 별찍기

--- 221124R/src/Test.java
+++ 221124R/src/Test.java
@@ -4,21 +4,11 @@ public class Test {
 
 	public static void main(String[] args) { 
 		
-		//하나씩 올라가는 for문
-		for(int i=1; i<=100; i++) {
-			System.out.println(i);
-		}
-		
-		//하나씩 감소하는 for문
-		for(int y=10; y>=0; y--) {
-			System.out.println(y);
-		}
-		
-		
-		//1부터 100까지 for문
-		for(int x=1; x<=100; x++) {
-			if (x%5==0 && x%6==0) { //5와 6의 배수 둘다 해당할떄 = 30의 배수
-				System.out.println(x);
+
+		for(int i = 1; i <= 15; i++) {
+			System.out.print("*");
+			if(i%5==0) {
+				System.out.println(); //별 찍은다음 enter (줄바꿈)의 역할
 			}
 		}
 		
