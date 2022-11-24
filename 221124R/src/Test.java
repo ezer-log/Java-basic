508aec4e3041f4e970f6e501e5e527d1b42a38de
ezer-log <ezer4611@gmail.com>
2022-11-24 17:15:02 +0900

    for문 - 반쪽삼각형 출력

--- 221124R/src/Test.java
+++ 221124R/src/Test.java
@@ -4,8 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		for(int i=1; i<=3; i++) { //행
-			for(int j=1; j<=5; j++) { //열
+		for(int i=0; i<=4; i++) { //행
+//			for(int j=0; j<=i; j++) { //열
+			for(int j=0; j<i+1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
