f67c87241356998f126f2cbad8bdc988197ebfe0
ezer-log <ezer4611@gmail.com>
2022-11-24 10:40:07 +0900

    1~100 정수 랜덤 뽑아 배수 확인

--- 221124R/src/Test.java
+++ 221124R/src/Test.java
@@ -4,26 +4,18 @@ public class Test {
 
 	public static void main(String[] args) { 
 		
-		int n = (int) (Math.random()*9) +4;
-		//4~12까지의 수 중에서 랜덤으로 값을 뽑아냄
 		
+		//1~100중에 정수 랜덤으로 뽑기
+		int n = (int) (Math.random()*99) +1;
 		
-		//1이 뽑아지면 "one"
-		//5가 뽑아지면 "five"
-		//6이 뽑아지면 "six"
-		
-		switch(n) {
-		case 1:
-			System.out.println("one");
-			break;
-		case 5:
-			System.out.println("five");
-			break;
-		case 6:
-			System.out.println("six");
-			break;
+		if(n%5==0)
+			System.out.println("n은 5의 배수");
+		else if(n%10==0)
+			System.out.println("n은 10의 배수");
+		else {
+			System.out.println("위 조건 다 아님");
 		}
-		System.out.println(n);
+
 	}
 
 }
