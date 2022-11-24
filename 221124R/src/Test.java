9ca42fb8bbc145100ed6388ec1ff5c64fe3c88a8
ezer-log <ezer4611@gmail.com>
2022-11-24 11:53:45 +0900

    for 문 - 구구

--- 221124R/src/Test.java
+++ 221124R/src/Test.java
@@ -5,11 +5,13 @@ public class Test {
 	public static void main(String[] args) { 
 		
 
-		for(int i = 1; i <= 15; i++) {
-			System.out.print("*");
-			if(i%5==0) {
-				System.out.println(); //별 찍은다음 enter (줄바꿈)의 역할
-			}
+		Scanner s = new Scanner(System.in);
+		System.out.println("몇단?");
+		
+		int dan = s.nextInt();
+		
+		for (int i=9; i>=1; i--) {
+			System.out.printf("%d * %d = %d \n", dan, i, dan*i);
 		}
 		
 	}
