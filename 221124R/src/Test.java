c4d9cb0b34822e4bd7e400d303274a6ea03f7404
ezer-log <ezer4611@gmail.com>
2022-11-24 17:17:34 +0900

    for문 - 반쪽삼각형 (아래로 오른쪽)

--- 221124R/src/Test.java
+++ 221124R/src/Test.java
@@ -4,9 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		for(int i=0; i<=4; i++) { //행
-//			for(int j=0; j<=i; j++) { //열
-			for(int j=0; j<i+1; j++) {
+		for(int i=0; i<5; i++) { //행
+//			for(int j=i; j<5; j++) { //열
+			for(int j=0; j<5-i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
