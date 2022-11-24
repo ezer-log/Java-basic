32243e89839292ef4a0e1ded4b7a0fce8069e926
ezer-log <ezer4611@gmail.com>
2022-11-24 10:07:10 +0900

    switch 예시 - 하나의 문자 입력

--- 221124R/src/Test.java
+++ 221124R/src/Test.java
@@ -2,9 +2,42 @@ import java.util.Scanner;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+
 		Scanner s = new Scanner(System.in);
 
-	}
+		System.out.print("글자 입력 : ");
+
+		char ch = s.next().charAt(0);
+
+		switch(ch) {
+
+			case 'm':
+
+				System.out.println("movie");
+
+				break;
+
+			case 's':	
+			
+				System.out.println("sing");
+
+				break;
+
+			case 'b':
+
+				System.out.println("book");
+
+				break;
+
+			default:
+
+				System.out.println("etc");
+
+				break;
+
+		}
+
+		}
 
 }
