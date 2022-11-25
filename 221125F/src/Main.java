618bf2247328f3922249f4814e4a117b4e08fb9d
user <ezer4611@gmail.com>
2022-11-25 12:14:10 +0900

    문제 3 - 중첩 반복문으로 5단부터 9단까지 출력

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -4,60 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-//		2. 시간과 물건 개수로 금액 할인해주는 프로그램
-//		(총 금액은 10만원으로 설정, 시간과물건 개수는 입력받기)
-//		14시에 마트를 방문하고 물건 개수가 3개 이하이면 5%로 할인
-//		18시에 마트를 방문하고 물건 개수가 5개 이하이면 10%로 할인
-//		20시에 마트를 방문하고 물건 개수가 10개 이하이면 20%로 할인
-		
-		Scanner s = new Scanner(System.in); 
-		int total = 100000;
-		
-		System.out.println("시간 입력:");
-		int time = s.nextInt();
-		System.out.println("물건 개수 입력:");
-		int count = s.nextInt();
-		
-		if (time<=14) {
-			if (count<=3) {
-				total *= 0.95;
+//		3. 중첩 반복문으로 5단부터 9단까지 출력
+//		
+		for(int i=0; i<10; i++) {
+			for (int j=5; i<=9; j++) {
+				System.out.printf("%d * %d = %d", j, i, i*j);
 			}
 		}
-		else if (time<=18) {
-			if (count<=5) {
-				total *= 0.9;
-			}
-		}
-		else if (time<=20) {
-			if (count<=10) {
-				total *= 0.8;
-			}
-		}
-		System.out.println(total);
-		
-		//case-switch 사용한다면
-		
-//		switch(time) {
-//		case 14 :
-//			if (count <= 3) {
-//				total *= 0.95;
-//			}
-//			break;
-//		case 18:
-//			if (count <= 5) {
-//				total *= 0.9;
-//			}
-//			break;
-//		case 20:
-//			if (count <= 10) {
-//				total *= 0.8;
-//			}
-//			break;
-//		default:
-//			System.out.println(total);
-//		}
-		
-		
-		
+
 	}
 }
