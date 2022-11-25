3220e120df794cc7aeaaf7bc2844bbfa29d2ce2a
user <ezer4611@gmail.com>
2022-11-25 12:14:39 +0900

    문제 4 - while문을 이용해서 1부터 100까지 정수 중 5의 배수의 총합을 출력

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -4,13 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-//		3. 중첩 반복문으로 5단부터 9단까지 출력
-//		
-		for(int i=0; i<10; i++) {
-			for (int j=5; i<=9; j++) {
-				System.out.printf("%d * %d = %d", j, i, i*j);
+//		4. while문을 이용해서 1부터 100까지 정수 중 5의 배수의 총합을 출력
+		
+		int i=0;
+		int total=0;
+		
+		while (i<=100) {
+			if (i%5==0) {
+				total+= i;
+				i++;
 			}
 		}
+		System.out.println(total);
+	
 
 	}
 }
