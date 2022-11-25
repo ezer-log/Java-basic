1db7003f7a790db93aed0ca23ceac7f64f09ab96
user <ezer4611@gmail.com>
2022-11-25 12:00:16 +0900

    문제2 - 시간과 물건 개수로 금액 할인해주는 프로그램

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -3,30 +3,59 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
 		
-//		1. 두 과목 점수를 입력하여 평균 출력하는 프로그램(평균은 소수점 이하 첫째자리까지 출력)
-//		출력 결과) 두 과목의점수 : 70 80
-//		평균 : 75.0
+//		2. 시간과 물건 개수로 금액 할인해주는 프로그램
+//		(총 금액은 10만원으로 설정, 시간과물건 개수는 입력받기)
+//		14시에 마트를 방문하고 물건 개수가 3개 이하이면 5%로 할인
+//		18시에 마트를 방문하고 물건 개수가 5개 이하이면 10%로 할인
+//		20시에 마트를 방문하고 물건 개수가 10개 이하이면 20%로 할인
 		
 		Scanner s = new Scanner(System.in); 
+		int total = 100000;
 		
-		System.out.println("첫과목 점수 입력:");
-		int a = s.nextInt();
-		System.out.println("두번째과목 점수 입력:");
-		int b = s.nextInt();
+		System.out.println("시간 입력:");
+		int time = s.nextInt();
+		System.out.println("물건 개수 입력:");
+		int count = s.nextInt();
 		
-		double avg = (a+b)/2.0;
+		if (time<=14) {
+			if (count<=3) {
+				total *= 0.95;
+			}
+		}
+		else if (time<=18) {
+			if (count<=5) {
+				total *= 0.9;
+			}
+		}
+		else if (time<=20) {
+			if (count<=10) {
+				total *= 0.8;
+			}
+		}
+		System.out.println(total);
 		
-		System.out.printf("두 과목의 점수 : %d %d \n평균 : %.1f", a, b, avg);
+		//case-switch 사용한다면
 		
-//		System.out.println("두 과목의 점수 : " + a + " "+ b + "\n 평균 : "+avg);
-		
-		
-		
-		
-		
-		
+//		switch(time) {
+//		case 14 :
+//			if (count <= 3) {
+//				total *= 0.95;
+//			}
+//			break;
+//		case 18:
+//			if (count <= 5) {
+//				total *= 0.9;
+//			}
+//			break;
+//		case 20:
+//			if (count <= 10) {
+//				total *= 0.8;
+//			}
+//			break;
+//		default:
+//			System.out.println(total);
+//		}
 		
 		
 		
