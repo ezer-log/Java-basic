6b42bcaefb32c589800a622c222d8c2062b3a286
user <ezer4611@gmail.com>
2022-11-25 18:04:17 +0900

    Array - 최대값 출력 (for문 안에 if문 사용)

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -6,20 +6,18 @@ public class Main {
 		
 		Scanner s = new Scanner(System.in);
 		
-		//5개 실수 입력할 배열을 생성하여
-		double a[] = new double[5];
-		int sum = 0;
+		//최대값 출력
 		
-		//5개 실수를 입력해 총합을 출력해라.
-		for (int i=0; i<a.length; i++) { //한계를 숫자로 지정해주기보다 배열.length
-			a[i] = s.nextDouble();
-			sum += a[i];
+		int a[] = new int[5];
+		int max=0;
+		
+		for (int i=0; i<a.length; i++) {
+			a[i] = s.nextInt();
+			if (max<a[i]) {
+				max = a[i];
+			}
 		}
-		System.out.println(a.length);
-		
-		//길이를 변수에 저장할 수도 있음.
-		int size = a.length;
-		System.out.println(size);
+		System.out.println(max);
 
 	}
 }
