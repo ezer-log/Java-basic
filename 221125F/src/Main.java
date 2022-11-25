edbed156bbb7cfba8fe29d7724091719fe593bfd
user <ezer4611@gmail.com>
2022-11-25 11:03:20 +0900

    while 무한반복문에서 break 사용, equals 메소드 사용

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -6,15 +6,19 @@ public class Main {
 		
 		Scanner s = new Scanner(System.in); //표준 입력스트림
 		
-		int sum=0, n=100;
-		
-		while(n!=0) { //0이 아닐때
-			System.out.println("입력해라 ");
-			n=s.nextInt(); //정수 입력함(0이 입력될 때까지)
-			sum+=n; //sum=sum+n; //내가 입력한 수 다 더함
+		while(true) {
+			//무한반복문 (무한루프) 멈춤 제어문자 -> break사용
+			
+			System.out.println("이름 입력");
+			String name = s.next();
+			
+			//문자열비교는 "==" 사용 못하고 equals 접근연산자(.)를 통해 사용해줘야 한다.
+			if(name.equals("한희")) {
+				break; //무한반복문 종료
+			}
+			System.out.println(name);
+			
 		}
-		
-		System.out.println(sum);
 		
 		s.close(); //닫아야지 메모리 공간 절약 가능
 		
