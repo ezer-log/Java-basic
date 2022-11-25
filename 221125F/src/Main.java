a5730ae378f7904af37b67c7d29a425ead7b3a86
user <ezer4611@gmail.com>
2022-11-25 17:55:59 +0900

    Array - 길이로 for문 돌리기

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -11,12 +11,15 @@ public class Main {
 		int sum = 0;
 		
 		//5개 실수를 입력해 총합을 출력해라.
-		for (int i=0; i<5; i++) {
+		for (int i=0; i<a.length; i++) { //한계를 숫자로 지정해주기보다 배열.length
 			a[i] = s.nextDouble();
 			sum += a[i];
 		}
-		System.out.println("합: "+sum);
-	
+		System.out.println(a.length);
+		
+		//길이를 변수에 저장할 수도 있음.
+		int size = a.length;
+		System.out.println(size);
 
 	}
 }
