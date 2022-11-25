a51f1a388c1c4016c622ad3edd6f00740ba3ef99
user <ezer4611@gmail.com>
2022-11-25 10:49:15 +0900

    while문 활용, scanner close

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -1,30 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		for(int i=0;i<5;i++) {
-			for(int j=0;j<4-i;j++) {
-				System.out.print(" ");
-			}
-			int n=1;
-			for(int j=0;j<(2*i)+1;j++) { //개수
-				System.out.print(n);
-				n++; //1을 기준으로 개수만큼 하나씩 증가
-			}
-			System.out.println();
+		Scanner s = new Scanner(System.in); //표준 입력스트림
+		
+		int sum=0, n=100;
+		
+		while(n!=0) { //0이 아닐때
+			System.out.println("입력해라 ");
+			n=s.nextInt(); //정수 입력함(0이 입력될 때까지)
+			sum+=n; //sum=sum+n; //내가 입력한 수 다 더함
 		}
 		
+		System.out.println(sum);
 		
-//		for(int i = 0; i < 5; i++) {
-//			for(int j = 1; j < 10; j++) {
-//				System.out.print((j >= 5 - i) && (j <= 5 + i) ? "*" : " ");
-//			}
-//			System.out.println();
-//		}
+		s.close(); //닫아야지 메모리 공간 절약 가능
+		
+		
+		
+		
+		
+		
 	}
-
 }
-
-
-//i*2 -1
