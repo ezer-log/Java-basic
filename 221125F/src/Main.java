c59d6b358c389a06c951966da65c18fcb7d662e2
user <ezer4611@gmail.com>
2022-11-25 11:43:46 +0900

    문제1 - 두과목 평균

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		
-		Scanner s = new Scanner(System.in); //표준 입력스트림
+//		1. 두 과목 점수를 입력하여 평균 출력하는 프로그램(평균은 소수점 이하 첫째자리까지 출력)
+//		출력 결과) 두 과목의점수 : 70 80
+//		평균 : 75.0
 		
-		System.out.println("정수 5개 입력");
-		int sum = 0;
+		Scanner s = new Scanner(System.in); 
 		
-//		int n=s.nextInt(); 이걸 5번 할 필요가 없다! for문 사용하자.
+		System.out.println("첫과목 점수 입력:");
+		int a = s.nextInt();
+		System.out.println("두번째과목 점수 입력:");
+		int b = s.nextInt();
 		
-		for(int i=0; i<5; i++) {    //횟수
-			int n=s.nextInt();      //5번 입력됨
-			if(n<0) {
-				continue;           //다시 올라가기 때문에 sum제외. (올라가서 반복문 다시실행)
-			}
-			else {                  //n>=0
-				sum+=n;
-			}
-		}
-		System.out.println(sum);
+		double avg = (a+b)/2.0;
+		
+		System.out.printf("두 과목의 점수 : %d %d \n평균 : %.1f", a, b, avg);
+		
+//		System.out.println("두 과목의 점수 : " + a + " "+ b + "\n 평균 : "+avg);
+		
+		
+		
+		
 		
 		
 		
