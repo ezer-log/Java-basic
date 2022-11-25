e3d871252dd900f519b7ea2b683e7b2fe3d299d3
user <ezer4611@gmail.com>
2022-11-25 12:55:43 +0900

    do-while - 1~20까지의 합

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -1,26 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 		
+		//1~20까지의 합
+		int sum=0;
+		int n=1;
 		
-//		5. while, random()을 이용해서 (x,y)형태로 출력하는데 x+y합이 5이면 실행을 멈추는 코드를 작성. 
-//		5가 아니면 난수 계속 발생시키고출력 (x,y 범위는 1부터 5까지)
-		
-		
-		while (true) {
-			int x = (int) (Math.random()*5)+1;
-			int y = (int) (Math.random()*5)+1;
-			
-			System.out.println("(" + x + " , " + y + ")");
-			
-			if (x+y ==5) {
-				break;
-			}
-		}
-		
-	
+		do {
+			sum+=n;
+			n++;
+		}while(n<=20);
+		System.out.println(sum);
+
 
 	}
 }
