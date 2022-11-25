69d35fdc506fd9cc2e169cc6fd5de5bce83af820
user <ezer4611@gmail.com>
2022-11-25 11:35:48 +0900

    for문 안에서 input 여러번 받기

--- 221125F/src/Main.java
+++ 221125F/src/Main.java
@@ -6,22 +6,21 @@ public class Main {
 		
 		Scanner s = new Scanner(System.in); //표준 입력스트림
 		
-		while(true) {
-			//무한반복문 (무한루프) 멈춤 제어문자 -> break사용
-			
-			System.out.println("이름 입력");
-			String name = s.next();
-			
-			//문자열비교는 "==" 사용 못하고 equals 접근연산자(.)를 통해 사용해줘야 한다.
-			if(name.equals("한희")) {
-				break; //무한반복문 종료
+		System.out.println("정수 5개 입력");
+		int sum = 0;
+		
+//		int n=s.nextInt(); 이걸 5번 할 필요가 없다! for문 사용하자.
+		
+		for(int i=0; i<5; i++) {    //횟수
+			int n=s.nextInt();      //5번 입력됨
+			if(n<0) {
+				continue;           //다시 올라가기 때문에 sum제외. (올라가서 반복문 다시실행)
 			}
-			System.out.println(name);
-			
+			else {                  //n>=0
+				sum+=n;
+			}
 		}
-		
-		s.close(); //닫아야지 메모리 공간 절약 가능
-		
+		System.out.println(sum);
 		
 		
 		
